45b322422c562258eba97e6a9c1f130718dc50c0
nadilakatuuk <nadilakatuuk@github.com>
2020-06-10 15:50:45 +0800

    Pertemuan 5 dan Exercise_2

---
+++ Pertemuan5/FloatAndDouble/src/com/multiplatform/Main.java
@@ -0,0 +1,29 @@
+package com.multiplatform;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Minimum Float Value = " + myMinFloatValue);
+        System.out.println("Maximum Float VAlue = " + myMaxFloatValue);
+
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Minimum Double Value = " + myMinDoubleValue);
+        System.out.println("Maximum Double VAlue = " + myMaxDoubleValue);
+
+        int myIntegerNumber =5 / 3;
+        float myFloatNumber =5.00f / 3.00f;
+        double myDoubleNumber =5.00d / 3.00d;
+
+        System.out.println(myIntegerNumber);
+        System.out.println(myFloatNumber);
+        System.out.println(myDoubleNumber);
+
+         
+
+
+    }
+}
---
+++ Pertemuan5/exercise_2/src/com/multiplatform/Main.java
@@ -0,0 +1,13 @@
+package com.multiplatform;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    double poundsValue = 500d;
+	    double convertedKilograms = poundsValue * 0.45359237d;
+        System.out.println("Hasil Convert Kilogram = " + convertedKilograms);
+
+
+
+    }
+}
