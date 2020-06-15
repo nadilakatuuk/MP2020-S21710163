33e42920626206263619b98859a50f2a05617b8a
nadilakatuuk <nadilakatuuk@github.com>
2020-06-15 16:43:50 +0800

    Pertemuan 8 dan Exercise 4

---
+++ Pertemuan7/Exercise3/src/com/multiplatform/Main.java
@@ -0,0 +1,38 @@
+package com.multiplatform;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //No.1
+        System.out.println(toMilesPerHour(1.5));
+        System.out.println(toMilesPerHour(10.25));
+        System.out.println(toMilesPerHour(-5.6));
+        System.out.println(toMilesPerHour(25.42));
+        System.out.println(toMilesPerHour(75.144));
+
+        //No.2
+        printConversion(1.5);
+        printConversion(10.25);
+        printConversion(-5.6);
+        printConversion(25.42);
+        printConversion(75.144);
+
+    }
+    public static long toMilesPerHour(double kilometersPerHour){
+        if(kilometersPerHour <0){
+            return -1;
+        }
+        return Math.round(kilometersPerHour/1.609);
+    }
+    public static  void printConversion(double kilometersPerhour){
+        if(kilometersPerhour <0){
+            System.out.println("Invalid value");
+        }
+        else{
+            long milesPerHours = toMilesPerHour(kilometersPerhour);
+            System.out.println(kilometersPerhour + "km/h =" + milesPerHours + "mi/h");
+        }
+    }
+
+}
