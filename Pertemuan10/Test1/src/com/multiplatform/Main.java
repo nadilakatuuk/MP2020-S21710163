eda80e4d42f16ab867361a183a6a30c4cf186ec6
nadilakatuuk <nadilakatuuk@github.com>
2020-06-17 16:00:03 +0800

    Pertemuan 10 Test 1

---
+++ Pertemuan10/Test1/src/com/multiplatform/Main.java
@@ -0,0 +1,48 @@
+package com.multiplatform;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //No.1
+        System.out.println(shouldWakeUp(true, 1));
+        System.out.println(shouldWakeUp(false, 2));
+        System.out.println(shouldWakeUp(true, 8));
+        System.out.println(shouldWakeUp(true, -1));
+
+        //No.2
+        System.out.println(isCatPlaying(true,10));
+        System.out.println(isCatPlaying(false,36));
+        System.out.println(isCatPlaying(false, 35));
+    }
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (barking) 
+            if (hourOfDay < 0 || hourOfDay > 23) {
+                return false;
+            } else if (hourOfDay < 8 || hourOfDay > 22) {
+                return true;
+            } else {
+                return false;
+            }
+            else{
+                return false;
+            }
+
+    }
+    public static boolean isCatPlaying(boolean summer, int temperature){
+        if (summer){
+            if (temperature >= 25 && temperature <= 45){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else if (temperature >= 25 && temperature <= 35){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
