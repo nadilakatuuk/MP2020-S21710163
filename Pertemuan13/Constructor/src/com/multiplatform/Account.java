b32a23cebcc13aa41f3a869dfa3452ece74f4e83
nadilakatuuk <nadilakatuuk@github.com>
2020-06-30 16:19:53 +0800

    update

--- Pertemuan13/Constructor/src/com/multiplatform/Account.java
+++ Pertemuan13/Constructor/src/com/multiplatform/Account.java
@@ -3,64 +3,82 @@ package com.multiplatform;
 public class Account {
     private String number;
     private double balance;
-    private String customerName;
+    private String costumerName;
     private String emailAddress;
     private String phoneNumber;
 
+    //Constructor
     public Account(){
-        this("99999999",0.0,"Default Name", "Default Email" , "0811111111");
+        //System.out.println("Constructor without parameter is running");
+        this("99999999", 0.0, "Default name", "Default email", "Default phone number" );
     }
 
-    public Account(String number, double balance, String customerName, String emailAddress, String phoneNumber) {
+    public Account(String number, double balance, String customerName, String emailAddress, String phoneNumber){
         this.number = number;
         this.balance = balance;
-        this.customerName = customerName;
+        this.costumerName = customerName;
         this.emailAddress = emailAddress;
         this.phoneNumber = phoneNumber;
+    }
+
+    public Account(String costumerName, String emailAddress, String phoneNumber) {
+//        this.costumerName = costumerName;
+//        this.emailAddress = emailAddress;
+//        this.phoneNumber = phoneNumber;
+        this("999999", 0.0, costumerName, emailAddress, phoneNumber);
     }
 
     public String getNumber() {
         return number;
     }
 
-    public void deposit (double amount){
-        this.balance += amount;
-        System.out.println("Deposit of " + amount + " made. New balnce is " + this.balance);
+    public void deposit(double amount){
+        this.balance += amount; //this.balance = this.balance + amount;
+        System.out.println("Deposit of " + amount + " made. Now balance is " + this.balance);
     }
 
     public void withdrawal (double amount){
-        if(this.balance < 0){
+        if(this.balance - amount < 0){
             System.out.println("Insufficient funds. Withdrawal not processes");
-        }else {
+        }else{
             this.balance -= amount;
-            System.out.println("Withdrawal of " + amount + " processed, Remaining balance is " + this.balance);
+            System.out.println("Withdrawal of " + amount + " processed. Remaining balance is " + this.balance);
         }
     }
-    
+
+//    public void setNumber(String number) {
+//        this.number = number;
+//    }
 
     public double getBalance() {
         return balance;
     }
 
+//    public void setBalance(double balance) {
+//        this.balance = balance;
+//    }
 
-
-    public String getCustomerName() {
-        return customerName;
+    public String getCostumerName() {
+        return costumerName;
     }
 
-
+//    public void setCostumerName(String costumerName) {
+//        this.costumerName = costumerName;
+//    }
 
     public String getEmailAddress() {
         return emailAddress;
     }
 
-
+//    public void setEmailAddress(String emailAddress) {
+//        this.emailAddress = emailAddress;
+//    }
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
-
+//    public void setPhoneNumber(String phoneNumber) {
+//        this.phoneNumber = phoneNumber;
+//    }
 }
-
-
--- Pertemuan13/Constructor/src/com/multiplatform/Main.java
+++ Pertemuan13/Constructor/src/com/multiplatform/Main.java
@@ -3,13 +3,28 @@ package com.multiplatform;
 public class Main {
 
     public static void main(String[] args) {
-        Account bobsAccount = new Account("11111111", 200.0, "John", "john@mail.com", "081211111");
+        // write your code here
+        Account johnsAcount = new Account("1111111", 200.00, "John", "john@mail,com", "12345678");
+//        johnsAcount.setCostumerName("Bob");
+//        johnsAcount.setNumber("123456789");
+//        johnsAcount.setBalance(100.00);
+//        johnsAcount.setEmailAddress("bob@mail.com");
+//        johnsAcount.setPhoneNumber("(62)811324234");
 
+        System.out.println("Customer name: " + johnsAcount.getCostumerName());
+        System.out.println("Balance: " + johnsAcount.getBalance());
+        johnsAcount.deposit(50.0);
+        System.out.println("Balance: " + johnsAcount.getBalance());
+        johnsAcount.withdrawal(155.0);
 
-        System.out.println("Customer name : " +bobsAccount.getCustomerName());
-        System.out.println("Balance : " +bobsAccount.getBalance());
-        System.out.println("Email : " +bobsAccount.getEmailAddress());
-        bobsAccount.deposit(50.0);
-        bobsAccount.withdrawal(155.0);
+        //Object for default constructor
+        Account bobsAccount = new Account();
+        System.out.println("Customer name: " + bobsAccount.getCostumerName());
+        System.out.println("Balance: " + bobsAccount.getBalance());
+
+        Account mikesAccount = new Account("Mike", "mike@mail.com", "123123123");
+        System.out.println("Customer name: " + mikesAccount.getCostumerName());
+        System.out.println("Balance: " + mikesAccount.getBalance());
+
     }
 }
